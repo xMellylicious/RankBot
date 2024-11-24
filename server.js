ae180f2966247b9f77c61fc504cce8c86c8f046e
xMellylicious <marshm3lly@gmail.com>
2024-11-24 11:40:23 +0000

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+		___  ___ _____ _      _   __   __
+		|  \/  ||  ___| |    | |  \ \ / /
+		| .  . || |__ | |    | |   \ V / 
+		| |\/| ||  __|| |    | |    \ /  
+		| |  | || |___| |____| |____| |  
+		\_|  |_/\____/\_____/\_____/\_/  
+		
+			WRITTEN BY xMellylicious.
+					  
+			   ALL RIGHTS RESERVED   
+	
+*/
+
+// DotEnv Config
+require("dotenv").config()
+
