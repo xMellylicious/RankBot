e283bc745710d1a8b0ec041b712f9ab2cd53e904
xMellylicious <marshm3lly@gmail.com>
2024-12-13 12:57:39 +0000

    sun group checker, token check for the update function

--- middleware/roblox.middleware.ts
+++ middleware/roblox.middleware.ts
@@ -1,55 +1,76 @@
 import { Request, Response, NextFunction } from "express";
 import axios from "axios";
 import { config } from "../config"
+import groupsConfig from "../data/groups.json"
 
 const checkMainGroupRank = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        if (!req.header("User-Id")) {res.status(405).json({message: "User ID not provided"}); return;}
+        if (!req.header("User-Id")) {
+            res.status(405).json({message: "User ID not provided"});
+            return;
+        }
 
-        let isinGroup = false
         const resp = await axios({
             method: "get",
             url: `https://groups.roblox.com/v1/users/${req.header("User-Id")}/groups/roles`,
-            params: {}
-        })
+        });
+
+        let isinGroup = false;
 
         for (let data of resp.data.data) {
             if (data.group.id == config.mainGroup.id) {
-                isinGroup = true
+                isinGroup = true;
                 if (data.role.rank == config.mainGroup.rankId) {
-                    res.status(405).json({body: "Player is already ranked"})
+                    res.status(405).json({body: "Player is already ranked"});
                     return;
                 }
-                break
+                break;
             }
         }
 
         if (!isinGroup) {
-            res.status(404).json({body: "Player is not in main group"}); return;
+            res.status(404).json({body: "Player is not in main group"});
+            return;
         }
 
+        req["RobloxResp"] = resp.data.data;
         next();
     } catch (e) {
-        res.status(500).json({message:e.message})
-        return;
+        res.status(500).json({message: e.message});
     }
 }
 
-const checkSubGroupRank = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+const checkSubGroupRank = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
+        if (!req["RobloxResp"]) { res.status(500).json({message: "Roblox did not provide a response"}); return; }
 
+        console.log(req["RobloxResp"]);
+
+        for (let data of req["RobloxResp"]) {
+            for (const [groupId, groupDetails] of Object.entries(groupsConfig)) {
+                if (data.group.id == groupId) {
+                    if (data.role.rank >= groupDetails.rankId) {
+                        next();
+                        return;
+                    }
+                }
+            }
+        }
+
+        res.status(500).json({message: "Could not rank!"});
     } catch (e) {
-        res.status(500).json({message:e.message})
-        return;
+        res.status(500).json({message: e.message});
     }
 }
 
 const updateRank = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        res.status(200).json({body: "all is well"}); return;
-    } catch (e) {
+        if (!process.env.RBLX_TKN) {res.status(500).json({body: "Can't run the update function as no Roblox token was provided!"}); return;}
 
+        res.status(200).json({body: "all is well"});
+    } catch (e) {
+        res.status(500).json({message: e.message});
     }
 }
 
-export {checkMainGroupRank, checkSubGroupRank, updateRank}
+export { checkMainGroupRank, checkSubGroupRank, updateRank }
--- routes/rankCheck.route.ts
+++ routes/rankCheck.route.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { checkMainGroupRank, updateRank } from "../middleware/roblox.middleware";
+import { checkMainGroupRank, checkSubGroupRank, updateRank } from "../middleware/roblox.middleware";
 
 const router = express.Router()
 
-router.post('/rank', checkMainGroupRank, updateRank)
+router.post('/rank', checkMainGroupRank, checkSubGroupRank, updateRank)
 
 export default router
